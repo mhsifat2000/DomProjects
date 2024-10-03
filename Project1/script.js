aaf1aa9ced9df75596a221dbd24cda27b2537436
Mahmudul Hasan Sifat <142844262+mhsifat2000@users.noreply.github.com>
2024-10-03 22:38:26 +0600

    Update script.js

--- Project1/script.js
+++ Project1/script.js
@@ -1,20 +1,49 @@
 window.onload = () => {
-  main();
+      main(); 
 };
+let div = null;
 
 function main() {
-  const root = document.getElementById('root');
-  const btn = document.getElementById('btn');
+  const root = document.getElementById("root");
+  const btn = document.getElementById("btn");
+  const output = document.getElementById("output");
+  const copy = document.getElementById("copy");
 
-  btn.addEventListener('click', function () {
+  btn.addEventListener("click", function () {
     const bgColor = generateRgbColor();
     root.style.backgroundColor = bgColor;
+    output.value=bgColor;
+  });
+  copy.addEventListener("click", function () {
+    navigator.clipboard.writeText(output.value);
+    if (div!=null) {
+        div.remove();
+        div=null;
+    }
+    generateTostMassage(`${output.value} Copied`);
   });
 }
 
 function generateRgbColor() {
-  const red = Math.floor(Math.random() * 256); // Range should be 0-255 (inclusive)
+  const red = Math.floor(Math.random() * 256);
   const green = Math.floor(Math.random() * 256);
   const blue = Math.floor(Math.random() * 256);
-  return `rgb(${red}, ${green}, ${blue})`;
+  return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
+}
+
+function generateTostMassage(msg){
+     div = document.createElement("div")
+    div.innerText = msg; 
+    div.className ="tost-massage tost-massage-in"
+    div.addEventListener("click",function() {
+        div.classList.remove("tost-massage-in");
+        div.classList.add("tost-massage-out")
+        div.addEventListener("animationend",function(){
+            div.remove();
+            div=null;
+        })
+
+    })
+    document.body.appendChild(div);
+    
 }
