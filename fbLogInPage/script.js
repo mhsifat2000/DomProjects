cef5f26781a96c1d979673c1fed578b816af601e
Mahmudul Hasan Sifat <142844262+mhsifat2000@users.noreply.github.com>
2024-10-30 00:17:48 +0600

    Update script.js

--- fbLogInPage/script.js
+++ fbLogInPage/script.js
@@ -1,7 +1,5 @@
 const password = document.getElementById('password');
 const hideShow = document.getElementById('hide-show');
-let State =0;
-let value = password.value;
 password.addEventListener('focus',() => {
     hideShow.style.display='block'
     hideShow.src ="./hide.svg";  
@@ -11,18 +9,18 @@ password.addEventListener('focus',() => {
 
 
 function handelHideShow (){    
+    let State =0;
 hideShow.addEventListener('click',() =>{
        if(State==0) {
        hideShow.src = './show.svg';
        password.type='text';
        State = 1;  
-       console.log(value);  
+       
        }
        else if(State==1) {
        hideShow.src = './hide.svg';
        password.type='password';
        State= 0;  
-       console.log(value); 
-       }
+     }
   })
 }
